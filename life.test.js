0e6a7bd308b44627beca314d8487a05620c5d359
Chris Freeman <cwfreeman@gmail.com>
2022-03-21 18:06:47 -0500

    Basic tests and hopefully watching files

---
+++ life.test.js
@@ -0,0 +1,9 @@
+import {Life} from './life.js';
+
+test('counts zero alive neighbors in empty space', () => {
+    new Life([]).;
+});
+
+test('', () => { });
+test('', () => { });
+test('', () => { });
